ae4fe96eddb60a3b4ddcca992536f3aa37e42238
Daria Korznikova <128809740+Daria2386@users.noreply.github.com>
2023-06-20 15:14:58 +0200

    confirmation page

---
+++ src/components/Confirmation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Confirmation() {
+  return (
+    <div>
+      <section>
+      <h1>Payment order successfull</h1>
+    <ion-icon   name="checkmark-done-circle"></ion-icon>
+    <p>Booking details are:</p>
+    <p>You have choosen:</p>
+    <p>Amount of cleaners:</p>
+    <p>Duration</p>
+    <p>Date</p>
+     
+    </section>
+    </div>
+  )
+}
+
+export default Confirmation
